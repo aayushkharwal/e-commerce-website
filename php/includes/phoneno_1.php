8672dae1c5e50ba592004c2bd3f71f4dfd453892
aayushkharwal <63885022+aayushkharwal@users.noreply.github.com>
2020-04-18 13:59:13 +0530

    Update phoneno_1.php

--- php/includes/phoneno_1.php
+++ php/includes/phoneno_1.php
@@ -5,5 +5,5 @@ $customer_id=$_SESSION['uid'];
 $phoneno=$_POST['phoneno'];
 $sql="insert into phonenos values($customer_id,$phoneno);";
 mysqli_query($conn,$sql);
-echo '<meta http-equiv = "refresh" content = "0; url = http://192.168.99.100/includes/login_n.php" />';
+echo '<meta http-equiv = "refresh" content = "0; url = https://mywebsite-ecommerce.herokuapp.com/includes/login_n.php" />';
 ?>
