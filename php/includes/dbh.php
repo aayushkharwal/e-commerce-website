6d9a7a92e9fddd8598f0e028c961e21852645038
aayushkharwal <63885022+aayushkharwal@users.noreply.github.com>
2020-04-18 16:18:29 +0530

    Update dbh.php

--- php/includes/dbh.php
+++ php/includes/dbh.php
@@ -3,12 +3,13 @@
 
 //echo $_SESSION["uid"]."dbh<br>";
 //gives error first time for reasons unknown to me,works fine if the page is refreshed once
-	$host = 'db';
-	$user = 'devuser';
-	$password = 'devpass';
-	$db = 'test_db';
+	$host = 'ec2-176-34-97-213.eu-west-1.compute.amazonaws.com';
+	$user = 'zgvgapjagddovz';
+	$password = 'a266d3681f854db006391c59798d0583bd67ddf0ed27844b6f78f6e6d57858d5';
+	$db = 'dckbu2aghbqmsk';
+	$port = 5432;
 
-	$conn = new mysqli($host,$user,$password,$db);
+	$conn = new mysqli($host,$user,$password,$db,$port);
 	if ($conn->connect_error){
 		echo "Failed";
 	}?>	
