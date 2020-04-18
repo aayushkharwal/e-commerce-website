deebae3773173d6ffd2e161ee49457494ac377e9
aayushkharwal <63885022+aayushkharwal@users.noreply.github.com>
2020-04-18 13:54:42 +0530

    Update delcust.php

--- php/includes/delcust.php
+++ php/includes/delcust.php
@@ -5,6 +5,6 @@ $customer_id=$_SESSION["uid"];
 echo $customer_id;
 $sql="delete from customers where id='$customer_id';";
   	if(mysqli_query($conn,$sql)){
-  	echo '<meta http-equiv = "refresh" content = "0; url = http://192.168.99.100/" />';
+  	echo '<meta http-equiv = "refresh" content = "0; url = https://mywebsite-ecommerce.herokuapp.com/" />';
   }
   ?>
