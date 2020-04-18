0071fc3667f4fa6c85e536bf14f160bd81d58221
aayushkharwal <63885022+aayushkharwal@users.noreply.github.com>
2020-04-18 13:57:47 +0530

    Update logout.php

--- php/includes/logout.php
+++ php/includes/logout.php
@@ -4,6 +4,6 @@ include_once 'dbh.php';
 session_unset();
 session_destroy();
 //echo $_SESSION["uid"]."dbh<br>";
-echo '<meta http-equiv = "refresh" content = "0; url = http://192.168.99.100/" />';
+echo '<meta http-equiv = "refresh" content = "0; url = https://mywebsite-ecommerce.herokuapp.com/" />';
 
 ?>
